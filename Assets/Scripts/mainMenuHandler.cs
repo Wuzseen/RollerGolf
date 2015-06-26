d15fc9e3af1d059f651d4508326f3b270f29d901
pjhodson <peter@thehodsons.org>
2015-06-26 11:14:21 -0700

    basic main menu scene. fixed project serialization. starter game scene. golf ball prefab.

---
+++ Assets/Scripts/mainMenuHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenuHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadGameScene() {
+		Application.LoadLevel(1);
+	}
+}
