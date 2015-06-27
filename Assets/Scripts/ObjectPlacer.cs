1c844c0d18407d2a14f432dfc72a6de8ca7caa29
Timothy Day <the.tday@gmail.com>
2015-06-27 00:21:06 -0400

    Some tweening for object shelf

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -2,8 +2,10 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 
 public class ObjectPlacer : MonoBehaviour {
+	public RectTransform shelf;
 
 	[System.Serializable]
 	class placableObject : System.Object {
@@ -18,6 +20,32 @@ public class ObjectPlacer : MonoBehaviour {
 	public GameObject guiAddObjectPrefab;
 	public GameObject guiGridLayout;
 
+	void OnEnable() {
+		CourseHandler.OnPlacementBegin += HandleOnPlacementBegin;
+		CourseHandler.OnPlacementEnd += HandleOnPlacementEnd;
+	}
+
+	void OnDisable() {
+		CourseHandler.OnPlacementBegin -= HandleOnPlacementBegin;
+		CourseHandler.OnPlacementEnd -= HandleOnPlacementEnd;
+	}
+
+
+
+	void HandleOnPlacementEnd () {
+		float tweenDist = -shelf.rect.height;
+		shelf.DOAnchorPos(new Vector2(0f,tweenDist),.2f).SetRelative(true);
+	}
+	
+	void HandleOnPlacementBegin () {
+		float tweenDist = -shelf.rect.height;
+		shelf.DOAnchorPos(new Vector2(0f,tweenDist),.2f).SetRelative(true);
+	}
+
+	void Awake () {
+		shelf.anchoredPosition = shelf.anchoredPosition + new Vector2(0f,shelf.rect.height);
+	}
+
 	// Use this for initialization
 	void Start () {
 		for(int i = 0; i < objects.Length; i++)
@@ -28,10 +56,5 @@ public class ObjectPlacer : MonoBehaviour {
 			temp.GetComponent<AddObjectGUIHandler>().setGameObject(objects[i].prefab);
 			temp.transform.SetParent(guiGridLayout.transform, false);
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
