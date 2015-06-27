f32e051ddd1e063b0531b6e0d5a45a1b7dcb65e3
pjhodson <peter@thehodsons.org>
2015-06-26 22:57:45 -0700

    extra stuff

---
+++ Assets/Scripts/SandTrapHover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SandTrapHover : MonoBehaviour {
+
+	public float hoverAmount = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newY = Mathf.Sin (Time.time);
+		float newX = Mathf.Sin (2.3f * Time.time);
+
+		Vector3 hoverPos = new Vector3(newX * 0.5f, newY * hoverAmount, 0);
+
+		this.transform.position += hoverPos * Time.deltaTime;
+	}
+}
