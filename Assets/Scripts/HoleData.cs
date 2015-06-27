3a364d0595fd4a9c08f77d2015b802d10096c00f
Timothy Day <the.tday@gmail.com>
2015-06-26 21:45:35 -0400

    Dependencies added. HOle picker ui started.

---
+++ Assets/Scripts/HoleData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HoleData : MonoBehaviour {
+	public string HoleName {
+		get {
+			return nameField.text;
+		}
+		set {
+			nameField.text = value;
+		}
+	}
+
+	[SerializeField]
+	private Text nameField;
+
+	public int Par;
+
+	public int Score;
+}
