eed447d1cf6f991ff8e4a43a298bf7e73f9c57df
Tim Day <the.tday@gmail.com>
2015-06-29 02:30:24 -0400

    Basic sfx handling; water splashes

---
+++ Assets/Scripts/FX/SFX.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFX : MonoBehaviour {
+    private AudioSource source;
+
+    public AudioClip[] waterSplashes;
+
+	void Awake () {
+        source = this.GetComponent<AudioSource>();
+        Ball.OnWaterHit += Ball_OnWaterHit;
+    }
+
+    void OnDestroy() {
+        Ball.OnWaterHit -= Ball_OnWaterHit;
+    }
+
+    void Ball_OnWaterHit(Vector3 point) {
+        PlayFX(waterSplashes[Random.Range(0, waterSplashes.Length)]);
+    }
+
+    void PlayFX(AudioClip clip) {
+        source.clip = clip;
+        source.Play();
+    }
+}
