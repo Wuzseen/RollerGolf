ddd8b1525443ebd1f6f7ba9c6fa126f821f7ac9f
Tim Day <the.tday@gmail.com>
2015-06-29 02:21:52 -0400

    Water particles!

---
+++ Assets/Scripts/Game/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+    public delegate void BallCollisionEvent(Vector3 point);
+    public static event BallCollisionEvent OnWaterHit;
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+    
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag(GameConsts.TAG_WATER)) {
+            Raise(OnWaterHit, this.transform.position);
+        }
+    }
+
+    void Raise(BallCollisionEvent anEvent, Vector3 point) {
+        if (anEvent != null) {
+            anEvent(point);
+        }
+    }
+}
