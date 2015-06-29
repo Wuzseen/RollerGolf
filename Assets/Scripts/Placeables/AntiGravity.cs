3c7684b7c27f3595484628c9786a4a38a8ae6b0e
Timothy Day <the.tday@gmail.com>
2015-06-29 14:03:28 -0400

    AntiGrav sfx

--- Assets/Scripts/Placeables/AntiGravity.cs
+++ Assets/Scripts/Placeables/AntiGravity.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AntiGravity : MonoBehaviour {
+public class AntiGravity : Placeable {
 
 	public float antiGravForce = 20.0f;
 
@@ -15,10 +15,15 @@ public class AntiGravity : MonoBehaviour {
 	
 	}
 
+	protected override void OnCollisionEnter2D (Collision2D col) {
+		base.OnCollisionEnter2D (col);
+	}
+
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if(col.gameObject.tag == "ball")
 		{
+			base.OnCollisionEnter2D(null);
 			col.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
 			col.gameObject.GetComponent<Rigidbody2D>().AddForce(this.transform.up * antiGravForce, ForceMode2D.Impulse);
 		}
