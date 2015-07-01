9c1a661f44879e992702448672c9c3e1b9712e9f
pjhodson <peter@thehodsons.org>
2015-07-01 15:02:26 -0700

    Planet Placeable created. Script may need to be edited to find ball onHoleBegin. GUI needs to be edited to allow for placement. This one should have a keep upright check on it. Need to add in ability to select which items are available on a given hole/course.

---
+++ Assets/Scripts/Placeables/Planet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+    private float myMass;
+
+    private GameObject nBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        myMass = this.GetComponent<Rigidbody2D>().mass;
+        nBody = GameObject.Find("Ball");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float euclidDist = Mathf.Sqrt(Mathf.Pow((nBody.transform.position.x - transform.position.x), 2) + Mathf.Pow((nBody.transform.position.y - transform.position.y), 2) + Mathf.Pow((nBody.transform.position.z - transform.position.z), 2));
+
+        Vector3 dir = this.transform.position - nBody.transform.position;
+        //transform.LookAt(nBody.transform.position);
+        float newtonForce = ((myMass * nBody.GetComponent<Rigidbody2D>().mass) / Mathf.Pow(euclidDist, 2));
+
+        Vector3 appliedForce = Mathf.Round(newtonForce) * dir;
+        appliedForce.z = 0;
+
+        if (!float.IsNaN(appliedForce.x) && !float.IsNaN(appliedForce.y) && !float.IsNaN(appliedForce.z) &&
+            !float.IsInfinity(appliedForce.x) && !float.IsInfinity(appliedForce.y) && !float.IsInfinity(appliedForce.z) &&
+            !float.IsNegativeInfinity(appliedForce.x) && !float.IsNegativeInfinity(appliedForce.y) && !float.IsNegativeInfinity(appliedForce.z))
+        {
+            nBody.GetComponent<Rigidbody2D>().AddForce(appliedForce);
+        }
+    }
+}
