72bfbfc8caa0bd7402f6b4eda21f6c8d21ce8e60
pjhodson <peter@thehodsons.org>
2015-07-02 12:20:36 -0700

    increased par on FOTV. started work on dev course loader to examine bugs on specific holes

---
+++ Assets/Scripts/Game/DevCourseLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevCourseLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+       //Debug.Log(FetchKey());
+
+        if(Input.GetKeyDown(KeyCode.LeftShift)) {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Debug.Log("yo");
+            }
+        }
+
+	
+	}
+}
